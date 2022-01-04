0596a45bf029feedd50f01220c9ec5d4610472b4
Adam Whittingham <adam.whittingham@gmail.com>
2022-01-04 22:56:16 +0000

    Show path in statusline

--- lua/config/statusline.lua
+++ lua/config/statusline.lua
@@ -1,1 +1,27 @@
-require('feline').setup()
+local components = require('feline.presets')["default"]
+
+components.active[1][3] = {
+    provider = {
+      name = "file_info",
+      opts = {
+        type = 'full-path',
+        colored_icon = false,
+      }
+    },
+    short_provider = {
+        name = 'file_info',
+        opts = {
+          type = 'short-path',
+          colored_icon = false,
+        }
+    },
+    hl = {
+        fg = 'white',
+        bg = 'oceanblue',
+        style = 'bold'
+    },
+    left_sep = {' ', 'slant_left'},
+    right_sep = {'slant_right_2', ' '},
+}
+
+require('feline').setup(components)
