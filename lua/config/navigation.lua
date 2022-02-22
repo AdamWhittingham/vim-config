91aee59e172a849bfbbe2bf9ce4919697e7cbd4c
Adam Whittingham <adam.whittingham@gmail.com>
2022-02-22 21:01:39 +0000

    Add buffer delete on c-d

--- lua/config/navigation.lua
+++ lua/config/navigation.lua
@@ -61,6 +61,7 @@ telescope.setup {
         ["<M-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
         ["<C-l>"] = actions.complete_tag,
         ["<C-_>"] = actions.which_key, -- keys from pressing <C-/>
+        ["<c-d>"] = actions.delete_buffer,
       },
 
       n = {
