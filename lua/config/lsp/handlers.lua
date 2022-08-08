e67efaedcba13db6548070150af0827d62748354
Adam Whittingham <ad@cronofy.com>
2022-08-08 14:49:18 +0100

    Prevent error when LSP does not support highlight

--- lua/config/lsp/handlers.lua
+++ lua/config/lsp/handlers.lua
@@ -47,20 +47,23 @@ M.on_attach = function(client, bufnr)
 
   vim.cmd [[ command! Format execute 'lua vim.lsp.buf.formatting_seq_sync()' ]]
 
-  vim.api.nvim_create_augroup("lsp_document_highlight", {clear = true})
-  vim.api.nvim_clear_autocmds { buffer = bufnr, group = "lsp_document_highlight" }
-  vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
-    group = "lsp_document_highlight",
-    buffer = bufnr,
-    callback = vim.lsp.buf.document_highlight,
-    desc = "Highlight usages",
-  })
-  vim.api.nvim_create_autocmd("CursorMoved", {
-    group = "lsp_document_highlight",
-    buffer = bufnr,
-    callback = vim.lsp.buf.clear_references,
-    desc = "Clear highlighted References",
-  })
+  local cap = client.resolved_capabilities
+  if cap.document_highlight then
+    vim.api.nvim_create_augroup("lsp_document_highlight", {clear = true})
+    vim.api.nvim_clear_autocmds { buffer = bufnr, group = "lsp_document_highlight" }
+    vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+      group = "lsp_document_highlight",
+      buffer = bufnr,
+      callback = vim.lsp.buf.document_highlight,
+      desc = "Highlight usages",
+    })
+    vim.api.nvim_create_autocmd("CursorMoved", {
+      group = "lsp_document_highlight",
+      buffer = bufnr,
+      callback = vim.lsp.buf.clear_references,
+      desc = "Clear highlighted References",
+    })
+  end
 end
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
