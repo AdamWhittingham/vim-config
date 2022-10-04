4ef0f0c50c3a7582f9cf77fe7c3e70d3667ce525
Adam Whittingham <ad@cronofy.com>
2022-10-04 14:15:48 +0100

    Add yanky telescope extension

--- lua/config/text_manipulation.lua
+++ lua/config/text_manipulation.lua
@@ -6,6 +6,12 @@ require("yanky").setup({
     select = { action = nil },
     telescope = { mappings = nil },
   },
+  ring = {
+    history_length = 20,
+    storage = "shada",
+    sync_with_numbered_registers = false,
+    cancel_event = "update",
+  },
 })
 
 require('textcase').setup {}
