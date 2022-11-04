a9b51f1316ba11e7f31e605fff776d7f9c23f149
Adam Whittingham <ad@cronofy.com>
2022-11-04 13:03:01 +0000

    Add format and organize on saving go files

--- lua/config/languages/go.lua
+++ lua/config/languages/go.lua
@@ -1,23 +1,14 @@
-function org_imports(wait_ms)
-  local params = vim.lsp.util.make_range_params()
-  params.context = {only = {"source.organizeImports"}}
-  local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, wait_ms)
-  for _, res in pairs(result or {}) do
-    for _, r in pairs(res.result or {}) do
-      if r.edit then
-        vim.lsp.util.apply_workspace_edit(r.edit)
-      else
-        vim.lsp.buf.execute_command(r.command)
-      end
-    end
-  end
+local on_save_go = function()
+  vim.lsp.buf.format()
+  vim.lsp.buf.code_action({
+    context = {only = {"source.organizeImports"}},
+    apply = true,
+  })
 end
 
-local group = vim.api.nvim_create_augroup("lang-go", {})
-vim.api.nvim_create_autocmd({"BufWritePre"}, {
-  command = "lua vim.lsp.buf.formatting_seq_sync()",
-  pattern = {"*.go"},
-  group = group,
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = "*.go",
+  callback = on_save_go,
 })
 
 local has_dapgo, dapgo = pcall(require, "dap-go")
