a6b7c767826df29ca22ffb4c1f5495d4ac04da35
Adam Whittingham <adam.whittingham@gmail.com>
2022-11-04 21:09:05 +0000

    Fix snippets

--- lua/config/snippets.lua
+++ lua/config/snippets.lua
@@ -4,5 +4,5 @@ local ls = require("luasnip")
 ls.filetype_extend("ruby", {"rails"})
 
 require("luasnip.loaders.from_vscode").lazy_load({
-  paths = { vim.fn.stdpath('config') .. '/snippets', }
+  paths = { './snippets' }
 })
