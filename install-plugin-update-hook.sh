5bf1c65261221f7aed02acc70bb3ed1f1ee25fa6
Adam Whittingham <adam.whittingham@gmail.com>
2022-12-30 14:56:50 +0000

    Better install hook

--- install-plugin-update-hook.sh
+++ install-plugin-update-hook.sh
@@ -2,6 +2,10 @@
 
 pushd $(dirname $0)>/dev/null
 
+cat > ~/.config/nvim/.git/hooks/post-merge <<-EOF
+  nvim --headless "+Lazy! sync" +qa
+EOF
+
 chmod u+x ~/.vim/.git/hooks/post-merge
 
 popd > /dev/null
