84a38ba10eff96e010b72619f48428d772665c7a
Adam Whittingham <ad@cronofy.com>
2023-01-02 09:12:39 +0000

    Fix vale loading

--- lua/config/languages/english.lua
+++ lua/config/languages/english.lua
@@ -1,4 +1,9 @@
-require("vale").setup({
+local status_ok, vale = pcall(require, "vale")
+if not status_ok then
+  return
+end
+
+vale.setup({
   bin="/usr/local/bin/vale",
   vale_config_path="$HOME/.adshell/config/vale.ini",
 })
