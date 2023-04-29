beff421ddbb9d67df34eb24e375f92d51cb0e936
Adam Whittingham <adam.whittingham@gmail.com>
2023-04-29 20:08:21 +0100

    Use opts instead of config for surround'

--- lua/config/plugins.lua
+++ lua/config/plugins.lua
@@ -37,16 +37,10 @@ local plugins = {
     "kylechui/nvim-surround",
     version = "*", -- Use for stability; omit to use `main` branch for the latest features
     event = "VeryLazy",
-    config = function()
-      require("nvim-surround").setup({
-        aliases = { ["<"] = "t", },
-        keymaps = {
-          normal = "sa",
-          delete = "sd",
-          change = "sc",
-        },
-        })
-    end
+    opts = {
+      aliases = { ["<"] = "t", },
+      keymaps = { normal = "sa", delete = "sd", change = "sc", },
+    }
   },
 
   -------------------
