91f422764e7fc09767ce1ebffe037be1b74d452d
Adam Whittingham <adam.whittingham@gmail.com>
2023-08-15 14:05:06 +0100

    Add pylsp

--- lua/config/lsp/init.lua
+++ lua/config/lsp/init.lua
@@ -16,6 +16,10 @@ if vim.fn.executable('ruby') == 1 then
   table.insert(servers, "solargraph")
 end
 
+if vim.fn.executable('python3') == 1 then
+  table.insert(servers, "pylsp")
+end
+
 if vim.fn.executable('docker') == 1 then
   table.insert(servers, "dockerls")
   table.insert(servers, "docker_compose_language_service")
