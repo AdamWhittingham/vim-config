0851718a1e39db05a61ffb74a926232b8bbf38c9
Adam Whittingham <adam.whittingham@gmail.com>
2023-10-10 22:49:19 +0100

    Add wrap binding for live_grep

--- lua/config/telescope.lua
+++ lua/config/telescope.lua
@@ -1,4 +1,7 @@
-require('telescope').setup{
+local telescope = require("telescope")
+local lga_actions = require("telescope-live-grep-args.actions")
+
+telescope.setup{
   defaults = {
     mappings = {
       i = {
@@ -13,6 +16,15 @@ require('telescope').setup{
       override_generic_sorter = true,  -- override the generic sorter
       override_file_sorter = true,     -- override the file sorter
       case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
+    },
+    live_grep_args = {
+      auto_quoting = true, -- enable/disable auto-quoting
+      mappings = { -- extend mappings
+        i = {
+          ["<C-k>"] = lga_actions.quote_prompt(),
+          ["<C-i>"] = lga_actions.quote_prompt({ postfix = " --iglob " }),
+        },
+      },
     }
   }
 }
