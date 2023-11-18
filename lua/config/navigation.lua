e7fda7216b1005ee707a74191b00ee685f552017
Adam Whittingham <adam.whittingham@gmail.com>
2023-11-18 23:52:04 +0000

    Fold in other mini plugins

--- lua/config/navigation.lua
+++ lua/config/navigation.lua
@@ -18,11 +18,3 @@ let g:projectionist_heuristics ={
       \ }
       \}
 ]]
-
-require('mini.bracketed').setup({
-  comment    = { suffix = '#', options = {} },-- default 'c' clashes with next/prex change
-  oldfile    = { suffix = '', options = {} }, -- disable oldfiles to keep o free for future use
-  yank       = { suffix = '', options = {} }, -- disable yank as Yanky does a more complete job
-})
-require('mini.cursorword').setup()
-require('mini.jump').setup()
--- lua/config/plugins/mini.lua
+++ lua/config/plugins/mini.lua
@@ -3,7 +3,14 @@ return {
   config = function()
     require('mini.ai').setup()
     require('mini.align').setup()
+    require('mini.bracketed').setup({
+      comment    = { suffix = '#', options = {} },-- default 'c' clashes with next/prex change
+      oldfile    = { suffix = '', options = {} }, -- disable oldfiles to keep o free for future use
+      yank       = { suffix = '', options = {} }, -- disable yank as Yanky does a more complete job
+    })
     require('mini.comment').setup()
+    require('mini.cursorword').setup()
+    require('mini.jump').setup()
     require('mini.trailspace').setup()
   end
 }
