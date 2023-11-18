53092e0c61058f6f2fb5eab0f83c1174679e24b1
Adam Whittingham <adam.whittingham@gmail.com>
2023-11-18 22:31:36 +0000

    Convert lualine to split config

--- lua/config/plugins/lua_line.lua
+++ lua/config/plugins/lua_line.lua
@@ -1,96 +1,86 @@
-local function configure()
-  local colors = {
-    fg_a  = '#ffffff',
-    fg_b  = '#dddddd',
-    fg_c  = '#cccccc',
-    black = '#080808',
-    bg_a  = '#10408F',
-    bg_b  = '#002080',
-    bg_c  = '#081028',
-  }
+local colors = {
+  fg_a  = '#ffffff',
+  fg_b  = '#dddddd',
+  fg_c  = '#cccccc',
+  black = '#080808',
+  bg_a  = '#10408F',
+  bg_b  = '#002080',
+  bg_c  = '#081028',
+}
 
-  local ad_code = {
-    normal = {
-      a = { fg = colors.fg_a, bg = colors.bg_a },
-      b = { fg = colors.fg_b, bg = colors.bg_b },
-      c = { fg = colors.fg_c, bg = colors.bg_c },
-    },
+local ad_code = {
+  normal = {
+    a = { fg = colors.fg_a, bg = colors.bg_a },
+    b = { fg = colors.fg_b, bg = colors.bg_b },
+    c = { fg = colors.fg_c, bg = colors.bg_c },
+  },
 
-    inactive = {
-      a = { fg = colors.fg_a, bg = colors.bg_a },
-      b = { fg = colors.fg_b, bg = colors.bg_b },
-      c = { fg = colors.fg_c},
-    },
-  }
+  inactive = {
+    a = { fg = colors.fg_a, bg = colors.bg_a },
+    b = { fg = colors.fg_b, bg = colors.bg_b },
+    c = { fg = colors.fg_c},
+  },
+}
 
-  return {
-    options = {
-      theme = ad_code,
-      component_separators = '',
-      section_separators = { left = '', right = '' },
+local line_config = {
+  options = {
+    theme = ad_code,
+    component_separators = '',
+    section_separators = { left = '', right = '' },
+  },
+  sections = {
+    lualine_a = {
+      'branch',
     },
-    sections = {
-      lualine_a = {
-        {
-          'filename',
-          separator = { right = '', left = '' },
-          path = 4,
-          symbols = {
-            modified = '•',      -- Text to show when the file is modified.
-            readonly = '',
-            unnamed = '󰢤', -- Text to show for unnamed buffers.
-            newfile = '',     -- Text to show for newly created file before first write
-          },
+    lualine_b = {
+      {
+        'filename',
+        separator = { right = '', left = '' },
+        path = 4,
+        symbols = {
+          modified = '•',      -- Text to show when the file is modified.
+          readonly = '',
+          unnamed = '󰢤', -- Text to show for unnamed buffers.
+          newfile = '',     -- Text to show for newly created file before first write
         },
       },
-      lualine_b = {
-        'filesize',
-      },
-      lualine_c = {
-        { 'diagnostics', symbols = {error = '', warn = '', info = '', hint = ''}, update_in_insert = true },
-        { 'navic' },
-      },
-      lualine_x = {
-        { 'diff', symbols = {added = '', modified = '', removed = ''} },
-      },
-      lualine_y = {
-        'branch',
-      },
-      lualine_z = {
-        'progress',
-        { 'location', separator = { right = '' }, left_padding = 2 },
-      },
     },
-
-    inactive_sections = {
-      lualine_a = { 'filename' },
-      lualine_b = {},
-      lualine_c = {},
-      lualine_x = {},
-      lualine_y = {},
-      lualine_z = { 'location' },
+    lualine_c = {
+      { 'diagnostics', symbols = {error = '', warn = '', info = '', hint = ''}, update_in_insert = true },
+      { 'navic' },
     },
+    lualine_x = {
+      { 'diff', symbols = {added = '', modified = '', removed = ''} },
+    },
+    lualine_y = {
+      'filesize',
+    },
+    lualine_z = {
+      'progress',
+      { 'location', separator = { right = '' }, left_padding = 2 },
+    },
+  },
 
-    tabline = {},
-    extensions = {},
-  }
-end
+  inactive_sections = {
+    lualine_a = { 'filename' },
+    lualine_b = {},
+    lualine_c = {},
+    lualine_x = {},
+    lualine_y = {},
+    lualine_z = { 'location' },
+  },
+
+  tabline = {},
+  extensions = {},
+}
 
 return {
-  -- show code hierarchy of item under cursor
-  {
-    "SmiteshP/nvim-navic",
-    dependencies = "neovim/nvim-lspconfig",
-    opts = {
-      separator = " » ",
-    }
-  },
   {
     'nvim-lualine/lualine.nvim',
     dependencies = {
       'nvim-tree/nvim-web-devicons',
       "nvim-navic",
     },
-    config = configure()
+    opts = line_config,
   }
 }
--- lua/config/plugins/ui.lua
+++ lua/config/plugins/ui.lua
@@ -1,12 +1,39 @@
 return {
   {
-    "AdamWhittingham/vim-adcode-theme",                                       -- A bright colorscheme
+    "AdamWhittingham/vim-adcode-theme",
     lazy = false,
     priority = 1000,
     config = function() vim.cmd([[colorscheme adCode]]) end,
   },
-  'mbbill/undotree',                                                          -- Visualise the undo tree and make it easy to navigate
-  { "lewis6991/gitsigns.nvim", dependencies = { "nvim-lua/plenary.nvim" } },  -- Show git diff in the gutter
+
+  -- Telescope plus the plugins that make it faster
+  { 'nvim-telescope/telescope.nvim',
+    lazy = false,
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "BurntSushi/ripgrep",
+      "sharkdp/fd",
+      { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+      "nvim-telescope/telescope-dap.nvim",
+      "nvim-telescope/telescope-live-grep-args.nvim" ,
+    },
+  },
+
+
+  -- Show git diff in the gutter
+  {
+    "lewis6991/gitsigns.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+  },
+
+  -- show code hierarchy of item under cursor
+  {
+    "SmiteshP/nvim-navic",
+    dependencies = "neovim/nvim-lspconfig",
+    opts = {
+      separator = " » ",
+    }
+  },
 
    -- Show colour swatches in virtualtext
     {
@@ -19,30 +46,15 @@ return {
     }
   },
 
--- File operatons using a Vim buffer
+  -- File operatons using a Vim buffer
   {
     'stevearc/oil.nvim',
     opts = {},
     dependencies = { "nvim-tree/nvim-web-devicons" },
   },
 
-  { -- Quick and nice statusline
-    'nvim-lualine/lualine.nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
-    config = function () require('config.statusline') end,
-  },
+  { 'mbbill/undotree' },                                                      -- Visualise the undo tree and make it easy to navigate
   { "folke/which-key.nvim", lazy = false, config = true },                    -- Help learn/relearn/remember key bindings with a handy pop up
   { "akinsho/git-conflict.nvim", config = true },                             -- Bindings to speed up choosing ours/theres when resolving diffs
   { "kevinhwang91/nvim-bqf", ft = "qf" },                                     -- Better Quickfix formatting
-  { 'nvim-telescope/telescope.nvim',                                          -- Telescope plus the plugins that make it faster
-    lazy = false,
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "BurntSushi/ripgrep",
-      "sharkdp/fd",
-      { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
-      "nvim-telescope/telescope-dap.nvim",
-      "nvim-telescope/telescope-live-grep-args.nvim" ,
-    },
-  },
 }
