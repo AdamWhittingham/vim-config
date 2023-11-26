f18db9e018585978455fc929b97bed6e43d00159
Adam Whittingham <adam.whittingham@gmail.com>
2023-11-26 21:20:53 +0000

    Move debugging to split config

--- lua/config/plugins/lsp.lua
+++ lua/config/plugins/lsp.lua
@@ -62,11 +62,4 @@ return {
   },
 
   { 'j-hui/fidget.nvim', config = true,  tag = 'legacy' },        -- Show LSP progress feedback
-
-  -- Debugging
-  "mfussenegger/nvim-dap",                       -- Debug Anything Protocol bindings
-  { "rcarriga/nvim-dap-ui", dependencies = {"mfussenegger/nvim-dap"} },   -- Lovely UI for DAP output
-  "theHamsta/nvim-dap-virtual-text",             -- Show debug values in virtual text
-  "leoluz/nvim-dap-go",                          -- Bindings for Delve, the go debugger
-  "suketa/nvim-dap-ruby",                        -- Bindings for ruby-debug
 }
