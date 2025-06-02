8303fa86f53b7f1d3944d50ab6b5be60e9226e47
Adam Whittingham <ad@cronofy.com>
2025-06-02 11:01:37 +0100

    Add grug-far back in

--- lua/config/plugins/navigation.lua
+++ lua/config/plugins/navigation.lua
@@ -49,15 +49,24 @@ return {
     opts = { mapping = nil }
   },
 
+  -- Better find and replace
+  {
+    'MagicDuck/grug-far.nvim',
+    config = function()
+      require('grug-far').setup({});
+    end
+  },
+
+
   -- Show and search paths in JSON and Yaml
   {
     "jfryy/keytrail.nvim",
     dependencies = {
-        "nvim-treesitter/nvim-treesitter",
-        "nvim-telescope/telescope.nvim",
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-telescope/telescope.nvim",
     },
     config = function()
-        require("keytrail").setup()
+      require("keytrail").setup()
     end,
-}
+  }
 }
