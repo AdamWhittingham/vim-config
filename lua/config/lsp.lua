d131c963273ba20c2fcfc88dab846bd9b0a591de
Adam Whittingham <ad@cronofy.com>
2025-08-05 14:58:18 +0100

    Enable other lsps

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -3,4 +3,8 @@ vim.lsp.config('*', {
   root_markers = { '.git' },
 })
 
+vim.lsp.enable('bashls')
+vim.lsp.enable('jsonls')
+vim.lsp.enable('lua')
 vim.lsp.enable('solargraph')
+vim.lsp.enable('yamlls')
