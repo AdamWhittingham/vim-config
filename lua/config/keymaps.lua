664c884b64e7a0f43abaa1ab293395927ee4cc73
Ad Whittingham <ad@cronofy.com>
2025-09-22 14:52:35 +0100

    Update grep

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -43,7 +43,7 @@ wk.add({
   { "<leader>.",        cmd[[FzfLua buffers]],                        desc = "Switch buffers" },
   { "<leader>?",        cmd[[FzfLua keymaps]],                        desc = "Search vim keymaps", icon = "" },
   { "<leader>f",        cmd[[FzfLua files]],                          desc = "Find file" },
-  { "<leader>F",        cmd[[FzfLua live_grep_glob]],                 desc = "Find text" },
+  { "<leader>F",        cmd[[lua FzfLua.live_grep()]],                desc = "Find text" },
   { "<leader>G",        cmd[[FzfLua resume]],                         desc = "Resume search" },
   { "<leader>h",        cmd[[nohlsearch]],                            desc = "Toggle search highlight" },
   { "<leader>j",        cmd[[FzfLua jumps]],                          desc = "Show Jumplist" },
