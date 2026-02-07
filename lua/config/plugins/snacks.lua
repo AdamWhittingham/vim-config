7306e3d1cc483e75527b94bc5120bbe72093c32e
Ad Whittingham <adam.whittingham@gmail.com>
2026-02-07 16:46:38 +0000

    Update dashboard

--- lua/config/plugins/snacks.lua
+++ lua/config/plugins/snacks.lua
@@ -17,7 +17,7 @@ return {
         },
         sections = {
           { section = "header" },
-          { icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1, limit = 9 },
+          { icon = " ", title = "Recent Files", section = "recent_files", cwd = true, indent = 2, padding = 1, limit = 9 },
           { icon = " ", title = "Keymaps", section = "keys", indent = 2, padding = 1 },
           { section = "startup" },
         },
